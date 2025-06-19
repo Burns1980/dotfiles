7440b179c48626a300770ef720078d4fe0757223
Burns1980 <zburns1980@gmail.com>
2025-06-19 13:20:23 -0600

    init setup

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,8 @@
+alias gcr="git add . && git commit --verbose && git push origin"
+alias ga="git add ."
+alias gc="git commit --verbose -m"
+alias gpsho="git push origin"
+alias gplo="git pull origin"
+alias gl="git log"
+alias gs="git status"
+alias wfec='powershell.exe /c start .'
